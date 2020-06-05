0bcc2032d23d54db06400e39932677ae75e84f87
sankalp choudhary <sankalpchoudhary8@gmail.com>
2020-06-05 12:54:57 +0530

    Add contact us

---
+++ README.md
@@ -0,0 +1,3 @@
+#My Project that is completed by code with harry
+1)learn SQLalchemy
+2)jinja templating
---
+++ main.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import json
+# import simplejson
+from datetime import datetime
+
+local_server = True
+with open('config.json','r') as c:
+    params = json.load(c)["params"]
+
+
+app = Flask(__name__)
+
+if local_server:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Contect(db.Model):
+    srn = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    phone_num = db.Column(db.String(12), nullable=False)
+    msg = db.Column(db.String(300), nullable=False)
+    date = db.Column(db.String(50), nullable=True)
+
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+
+@app.route("/contact", methods = ['GET', 'POST'])
+def contect():
+    if(request.method=='POST'):
+        '''Add entry to the database'''
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+        entry = Contect(name=name, phone_num = phone, msg = message, date= datetime.now(),email = email )
+        db.session.add(entry)
+        db.session.commit()
+        return render_template('thank-you.html')
+    return render_template('contact.html')
+
+app.run(debug=True)
